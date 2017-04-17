2d8dc6cf6eecf2b687d3fe916af8d96b612f642b
axel669 <haryuu.hanekata@gmail.com>
2017-04-16 22:31:54 -0700

    added margin (top/bottom) to button

--- source/component/Button.js
+++ source/component/Button.js
@@ -17,8 +17,9 @@ componentStyleSheet.addStyles({
         borderRadius: 3,
         borderWidth: 0,
         overflow: 'hidden',
-        marginLeft: 5,
-        marginRight: 5
+        // marginLeft: 5,
+        // marginRight: 5
+        margin: 5
     },
     "doric-button:after": {
         content: '""',
