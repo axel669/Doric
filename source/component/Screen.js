c3b9eb6f97b9d7d441e9d885d80a7c1cdb48e070
axel669 <haryuu.hanekata@gmail.com>
2017-05-03 15:03:06 -0700

    added back button and menus

--- source/component/Screen.js
+++ source/component/Screen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import componentStyleSheet from 'source/util/app';
+import {CSS} from 'source/util/stylesheet';
 
+const animTime = 200;
 componentStyleSheet.addStyles({
     "doric-screen": {
         position: 'absolute',
@@ -23,7 +25,8 @@ componentStyleSheet.addStyles({
         alignItems: 'center',
         boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.25)',
         fontSize: 20,
-        zIndex: '+5'
+        zIndex: '+5',
+        overflow: 'visible'
     },
     "doric-screen-content": {
         position: 'absolute',
@@ -32,6 +35,52 @@ componentStyleSheet.addStyles({
         right: 0,
         bottom: 0,
         overflow: 'auto'
+    },
+    "doric-screen-menu": {
+        position: 'absolute',
+        top: 40,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: CSS.rgba(0, 0, 0, 0.2),
+        boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.25)',
+        zIndex: '+5',
+        transform: 'translateX(-100%)',
+        overflow: 'hidden',
+        opacity: 0,
+        transition: `opacity ${animTime}ms linear, transform 0ms linear 250ms`
+    },
+    "doric-screen-menu > div": {
+        transform: 'translateY(-100%)',
+        transition: `transform ${animTime}ms linear`
+    },
+    "doric-screen-menu[visible='true']": {
+        transform: 'translateX(0)',
+        opacity: 1,
+        transition: `opacity ${animTime}ms linear`
+    },
+    "doric-screen-menu[visible='true'] > div": {
+        transform: 'translateY(0%)'
+    },
+    ".doric-screen-menu-button": {
+        width: 45,
+        height: 40,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        color: 'white',
+        borderRadius: 0,
+        margin: 0
+    },
+    ".doric-screen-back-button": {
+        height: 40,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        color: 'white',
+        borderRadius: 0,
+        margin: 0,
+        fontSize: 14
     }
 });
 class Screen extends React.Component {
@@ -40,26 +89,49 @@ class Screen extends React.Component {
         this.state = {expanded: false};
     }
 
+    toggle = () => {
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    }
+
     render = () => {
         const {
-            title
+            title,
+            menu = null,
+            backText = null
+        } = this.props;
+        let {
+            onBack = null
         } = this.props;
         const {
             expanded
         } = this.state;
 
-        const menuStyle = {
-            display: expanded === true ? 'block' : 'none'
-        };
+        let menuButton = null;
+        let backButton = null;
+
+        if (onBack !== null || backText !== null) {
+            onBack = onBack || (() => App.nav.pop());
+            backButton = <Doric.IconButton icon="ion-chevron-left" text={backText} className="doric-screen-back-button" onTap={onBack} />;
+        }
+
+        if (menu !== null) {
+            menuButton = <Doric.IconButton className="doric-screen-menu-button" icon="ion-navicon" onTap={this.toggle} iconStyle={{fontSize: 28}} />;
+        }
 
         return (
             <doric-screen>
                 <doric-screen-title>
                     {title}
-                    {/* <doric-screen-menu style={menuStyle}>
-                        Test
-                    </doric-screen-menu> */}
+                    {backButton}
+                    {menuButton}
                 </doric-screen-title>
+                <doric-screen-menu visible={expanded}>
+                    <div style={{backgroundColor: '#4285f4'}}>
+                        {menu}
+                    </div>
+                </doric-screen-menu>
                 <doric-screen-content>
                     {this.props.children}
                 </doric-screen-content>
