cb78b4a28634170a3e9e7afa2ca392c9b925abe1
axel669 <haryuu.hanekata@gmail.com>
2017-05-03 15:02:38 -0700

    updated to control font size

--- source/component/Icon.js
+++ source/component/Icon.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import Button from 'source/component/button';
+
 import componentStyleSheet from 'source/util/app';
 import icons from 'source/util/icons';
 
 componentStyleSheet.addStyles({
     "doric-icon": {
         display: 'inline',
-        fontSize: 18,
-        fontFamily: "Ionic"
+        fontSize: 16,
+        fontFamily: "Ionic",
+        padding: 2
     }
 });
 const Icon = ({icon, className, ...passThrough}) => <doric-icon {...passThrough} class={className}>{icons[icon]}</doric-icon>;
-const IconButton = ({icon, text = "", children, ...props}) => <Button {...props} text={<Icon icon={icon} />}>{text}{children}</Button>;
+const IconButton = ({icon, iconStyle, text = "", children, ...props}) => <Button {...props} text={<Icon icon={icon} style={iconStyle} />}>{text}{children}</Button>;
 
 export {Icon, IconButton};
